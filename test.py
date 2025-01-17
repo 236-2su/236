77fdf70970db3be979ce6a0e9197262d4befd9fc
236-2su <lsw0152@naver.com>
2025-01-17 16:52:57 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -1,20 +1,14 @@
-import numpy as np
 import sys
 sys.stdin = open("input.txt", "r")
 
 T = int(input())
-result=np.zeros((N,N), dtype= int)
-for test_case in range(1, T + 1):
-    N = int(input())
-    for i in range(1, N+1):
-        for j in range(1, i+1):
-            if j== 1 or j==i:
-                result[i,j] = 1
-            else:
-                result[i,j] = result[i-1,j-1]+result[i-1,j]
 
-    print(f"#{test_case}")
-    for i in range(N):
-     	 print(" ".join(map(str,result[i,:i+1])))
-      
-      
+for test_case in range(1, T + 1):
+    N,M = map(int,input().split())
+    num =[]
+    for i in range(0,N):
+        row= list(map(int,input().split()))
+        num.append(row)
+print(N,M)
+        
+print(num)
